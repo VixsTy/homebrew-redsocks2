6e2dd0b5049b37c35b849ef5bbce34120e285ebb
Kevin LARQUEMIN <kevin.larquemin@continental-corporation.com>
2018-03-03 16:12:44 +0100

    adding brew services management

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # Redsocks2 homebrew formula
 
-This Homebrew formula allow your to build and install a redsocks2 on a MacOS environment.
+This Homebrew formula allow you to build and install a redsocks2 binary on a MacOS environment.
 
-The original redsocks is not compliant with MacOS PF, and support only FreeBSD PF.
-Worst, the redsocks Homebrew-core formula install an iptables compliant binary.
+## Difference with the homebrew-core redsocks formula ?
+
+The original [darkk/redsocks](https://github.com/darkk/redsocks) is not compliant with MacOS PF, and support only FreeBSD PF.
+Worst, the Homebrew-core redsocks formula install an iptables compliant binary.
+
+So i found an active fork [semigodking/redsocks](https://github.com/semigodking/redsocks) and add some fix to correctly build and working on MacOS
 
 Why do we need a redsocks working with pf ?
 * The best way to work with redsocks is to use pf to redirect tcp packets to redsocks.
--- redsocks2.rb
+++ redsocks2.rb
@@ -18,8 +18,8 @@ class Redsocks2 < Formula
 
   stable do
     url 'https://github.com/VixsTy/redsocks.git',
-      :using => GitNoDepthDownloadStrategy, :shallow => false, :tag => "0.7.1-macos"
-    version "0.7.1-macos"
+      :using => GitNoDepthDownloadStrategy, :shallow => false, :tag => "0.7.2-macos"
+    version "0.7.2-macos"
   end
 
   head do
@@ -35,5 +35,40 @@ class Redsocks2 < Formula
     system "git submodule update --init --recursive"
     system "make"
     bin.install "redsocks2"
+    man8.install "debian/redsocks.8"
+    etc.install "darwin-xnu/redsocks.conf"
   end
+
+  def caveats
+    "Edit #{etc}/redsocks.conf to configure redsocks"
+  end
+
+  plist_options :startup => true
+
+  def plist; <<~EOS
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple Computer//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/redsocks2</string>
+          <string>-c</string>
+          <string>#{etc}/redsocks.conf</string>
+        </array>
+        <key>KeepAlive</key>
+        <false/>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>StandardErrorPath</key>
+        <string>/dev/null</string>
+        <key>StandardOutPath</key>
+        <string>/dev/null</string>
+      </dict>
+    </plist>
+    EOS
+  end
+
 end
